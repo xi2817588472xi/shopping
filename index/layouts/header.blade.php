51815c7986dad215253e81a9c57c875bc5c30cc6
Demo <2817588472@qq.com>
2019-12-09 11:33:45 +0800

    前台样式

---
+++ index/layouts/header.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>美国站</title> 
+<meta name="keywords" content="美国站" />
+<meta name="description" content="" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=no" /> 
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+<link rel="stylesheet" rev="stylesheet" href="/index/css/base.css" type="text/css" media="screen">
+<link rel="stylesheet" rev="stylesheet" href="/index/css/usa.css" type="text/css" media="screen">
+<link rel="stylesheet" rev="stylesheet" href="/index/css/mobile.css" type="text/css" media="screen">
+<!--[if lt IE 9]> 
+<script src="/index/js/css3-mediaqueries.js"></script> 
+<![endif]-->
+<!--/*技术支持，小庄602842076     验证：商城技术支持*/
+/*技术支持，小庄602842076    验证：商城技术支持*/
+/*技术支持，小庄602842076    验证：商城技术支持*/
+/*技术支持，小庄602842076    验证：商城技术支持*/
+/*技术支持，小庄602842076    验证：商城技术支持*/
+/*https://shop116998991.taobao.com/*/
+/*https://shop116998991.taobao.com/*/
+/*https://shop116998991.taobao.com/*/-->
+
+<script type="text/javascript" src="/index/js/jquery.min.js"></script>
+
+</head>
+<body>
+    <div class="content">
+
+        <!--top-->
+        <div class="top mobile_none">
+            <div class="clear w90" id="top">
+                <div class="top_l">
+                    <a class="" href="https://favorite.taobao.com/popup/add_collection.htm?spm=a1z10.1-c.w5003-12555806454.1.l6Mu9Q&id=116998991&itemid=116998991&itemtype=0&sellerid=1079002304&scjjc=2&scene=taobao_shop" >收藏淘商城</a> <a class="" href="https://shop116998991.taobao.com/">在线客服</a><a href="https://shop116998991.taobao.com/">手机版</a></div>
+                <div class="fr">
+                    <div class="fl"><a href="register.html">注册</a>|<a href="login.html">登录</a></div>
+                        <div class="fr" style="padding-top:6px">
+                            <a href="#" class="qq_login" title="qq快速登录"><img src="/index/images/qq.gif" height="22"></a> 
+                            <a href="http://blog.sina.com.cn/webzmy" class="sina_login" title="新浪微博快速登录"><img src="/index/images/sina.png" height="22"></a>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <!--top-->
+
+        <!--header-->
+        <div class="header ">
+            <div class="clear w90" id="header">
+                <div class="mobile_logo"><a href="index.html">淘商城</a></div>
+                <div class="header_l mobile_none"><a href="index.html"><img src="/index/images/logo.png"></a></div>
+                <!--搜索-->
+                <div class="header_search">
+                    <div class="search clear">
+                        <div class="searchContainer">
+                            <div class="searchselect">
+                                <span id="type" typename="tuan">团购</span><a class="searchselectbtn" href="javascript:void(0);">&nbsp;</a>
+                                <ul id=selectTypeList>
+                                    <li typename="tuan"><a href="javascript:void(0);">团购</a></li>
+                                    <li typename="item"><a href="javascript:void(0);">值得买</a> </li>
+                                    <li typename="promo"><a href="javascript:void(0);">促销</a> </li>
+                                    <li typename="store"><a href="javascript:void(0);">商家</a> </li>
+                                    <li typename="topic"><a href="javascript:void(0);">社区</a> </li>
+                                    <li class=last></li>
+                                </ul>
+                            </div>
+                            <input id="search_key" type="text" placeholder="输入关键词" value=""/>
+                            <a id="searchbtn" href="javascript:void(0);"><span class="searchbtn">搜索</span></a>  
+                        </div>
+                    </div>                    
+                </div>
+                <!--搜索-->
+
+                <div class="header_shopping_car mobile_none">
+                    <div class="clear topcar">
+                        <div class="tuan_car_num fl"><a href="user/shopping-cart.html" >购物车 <span id="tuan_num">1</span> 件</a>
+                        </div>
+                        <div class="car_love fl"><a href="https://shop116998991.taobao.com" >我喜欢的</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <!--header-->
+
+        <!--nav-->
+        <div class="nav mobile_none">
+            <div class="w90">
+                <div class="clear nav_text">
+                    <div class="nav_l">
+                        <a href="index.html" class="index_i ">首页</a> 
+                        <a href="usa.html" class="on">美国站</a>
+                        <a href="usa.html" >日本站</a>
+                        <a href="usa.html" >澳洲站</a> 
+                        <a href="usa.html" >德国站</a>
+                        <a href="usa.html" >韩国站</a>
+                        <a href="s.html" >全球精选</a>
+                    </div>
+                    <!--<div class="nav_r">
+                        <a href="#" class="nav_zdm">全球值得买</a>
+                        <a href="#" class="nav_ship">全球转运</a>
+                        <a href="#" class="nav_travel" target="_blank">海外游</a>
+                    </div>-->
+                </div>
+            </div>
+        </div>
+        <!--nav-->
+
+        
+        <div class="p_nav_c" id="p_nav_c">
+            <p class="p_nav_title" id="p_nav_title"><a href="#" class="on">热门海淘</a></p>
+            <div class="index_product_class" id="index_product_class" style="display:none">
+                <ul class="clear">
+                <li>
+                <h3><a href="#">母婴</a></h3>
+                <a href="#" class="on">孕妇</a>
+                <a href="#">爱他美</a>
+                <a href="#">奶粉</a>
+                <a href="#">米粉</a>
+                <a href="#">麦肯齐</a>
+                <a href="#">nuby</a>
+                <a href="#" class="on">帕玛氏</a>
+                <a href="#">小蜜蜂</a>
+                <a href="#" >挪威小鱼</a>
+                <a href="#"  class="on">加州宝宝</a>
+                <a href="#">童年时光</a>
+                </li>
+                
+                <li>
+                <h3><a href="#">保健</a></h3>
+                <a href="#" class="on">GNC</a>
+                <a href="#">普丽普莱</a>
+                <a href="#">鱼油</a>
+                <a href="#">减肥</a>
+                <a href="#">维骨力</a>
+                <a href="#" class="on">葡萄籽</a>
+                <a href="#">自然之宝</a>
+                <a href="#" class="on">MACA</a>
+                <a href="#">助睡眠</a>
+                <a href="#">维生素</a>
+                </li>
+                
+                <li>
+                <h3><a href="#">生活</a></h3>
+                <a href="#" class="on">膳魔师</a>
+                <a href="#">柠檬杯</a>
+                <a href="#">艾维诺</a>
+                <a href="#">紫草膏</a>
+                <a href="#">泡芙</a>
+                <a href="#" class="on">泡泡浴</a>
+                <a href="#">沐浴露</a>
+                
+                </li>
+                
+                <li style="border:none">
+                <h3><a href="#">更多</a></h3>
+                <a href="#">轻奢包</a>
+                <a href="#">coach</a>
+                <a href="#">MK</a>
+                <a href="#">科颜氏</a>
+                <a href="#">兰芝</a>
+                <a href="#">花王</a>
+                <a href="#">乐高</a>
+                <a href="#">耳机</a>
+                <a href="#"  class="on">婴儿碗</a>
+                <a href="#">马油</a>
+                <a href="#">九朵云</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+
+       
+        
